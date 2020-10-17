6339b9e75a1fd580a6f51d2fc8cbc3b72d7f6f4d
roken47 <mradeboer7@gmail.com>
2020-10-16 22:04:09 -0500

    boilerplateHTML + some js

---
+++ main.js
@@ -0,0 +1,9 @@
+// const body = document.querySelector("body");
+// document.body.append(
+//   <img src="https://images.unsplash.com/photo-1602868503797-cba991a7e541?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80" />
+// );
+// const body = document.querySelector("body");
+maskLady = new Image(450, 650);
+maskLady.src =
+  "https://images.unsplash.com/photo-1602868503797-cba991a7e541?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80";
+document.body.appendChild(maskLady);
